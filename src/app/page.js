db325c435a62fc5100f697b89b078efee8727867
gulliver-t-suzuki <gulliver.t.suzuki@gmail.com>
2023-05-02 17:27:44 +0900

    First commit

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+      </div>
+    </main>
+  );
+}
